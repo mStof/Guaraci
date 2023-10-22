7828be350f2198469b2e1d23a159c66c129d5c23
Mateus <137577094+mStof@users.noreply.github.com>
2023-10-22 01:25:41 -0300

    Nhaaaaa inferno

---
+++ Animação.js
@@ -0,0 +1,92 @@
+const divSol = document.querySelector('#sol');
+gsap.to('.sol_svg', {
+    rotate:360,
+    repeat:-1,
+    ease: "none",
+    duration: 15,
+    
+})
+
+
+
+const voltar = () => {
+    var tlVoltar = gsap.timeline()
+
+    tlVoltar.to('body', {
+        background:'url(img/Index/Montanha_direita.svg) no-repeat right bottom, url(img/Index/Montanha_esquerda.svg) no-repeat -110px 120% var(--primary-green)',
+        overflow:'auto',
+        duration:0,
+    })
+    tlVoltar.to('nav',{
+        opacity:0,
+        duration:.3
+    }, '.sol_svg-=1')
+
+    // window.scrollY = 0
+    
+    tlVoltar.to('.sol_svg', {
+        scale:1,
+        duration:1,
+    })
+    tlVoltar.to('.sol_svg', {
+        x:0+'%',
+        duration:1,
+    })
+    tlVoltar.to('.sol_svg', {
+        yPercent:'none',
+        top:35+"%",
+        cursor:'pointer',
+        duration:1,
+    })
+    tlVoltar.to(divSol, {
+        position:'relative',
+        y:'none',
+        height:'auto',
+        duration: 0,
+    })
+    tlVoltar.to('.content_inicial, #sol h1', {
+        opacity: 1,
+        duration:1,
+    })
+}
+
+const moverSol = () => {
+    var tlSol = gsap.timeline()
+    tlSol.to('.content_inicial, #sol h1', {
+        opacity: 0,
+        duration:1,
+    })
+    
+    tlSol.to(divSol, {
+        position:'fixed',
+        y:0,
+        height:100+'vh',
+        duration: 0,
+    })
+    tlSol.to('.sol_svg', {
+        yPercent:-50,
+        top:50+'%',
+        cursor:'auto',
+        duration:1,
+    })
+    tlSol.to('.sol_svg', {
+        x:50+'%',
+        duration:1,
+    })
+    tlSol.to('.sol_svg', {
+        scale:1.4,
+        duration:1,
+    })
+    tlSol.to('nav',{
+        opacity:1,
+        duration:.3
+    }, '.sol_svg-=1')
+    tlSol.to('body', {
+        backgroundImage:'none',
+        overflow:'auto',
+        duration:0,
+    })
+}
+
+divSol.addEventListener('click', moverSol)
+document.querySelector('.material-symbols-rounded').addEventListener('click', voltar)
---
+++ select.js
@@ -0,0 +1,26 @@
+const select_lingua = document.querySelector("select#select_lingua");
+const select_mode = document.querySelector("select#select_modo");
+      
+select_lingua.addEventListener("change", () => {
+let Flags = {
+        flags: ["img/Index/Brazil_flag.svg", "img/Index/USA_Flag.svg"],
+        alt: ["Brazil_flag", "USA_flag"],
+        text: ["Começar", "Start"]
+        };
+        
+        document.querySelector("img.flag").src = (select_lingua.value == "US") ? Flags.flags[1] : Flags.flags[0];
+        document.querySelector("img.flag").alt = (select_lingua.value == "US") ? Flags.alt[1] : Flags.alt[0];
+        document.querySelector("h1.iniciar").innerHTML = (select_lingua.value == "US") ? Flags.text[1] : Flags.text[0];
+    });
+    
+    select_mode.addEventListener("change", () => {
+        document.querySelector(":root").classList.toggle("dark");
+    });
+    
+    // EsterEggs
+select_lingua.addEventListener("change", () => {
+    if (select_lingua.value == "PT") {
+        document.querySelector("img.flag").addEventListener("mousedown", () => {
+            setTimeout(() => { document.querySelector("img.flag").src = "img/Index/Brazil_flag_PT.svg";}, 1000 * 5);
+    })}
+})
