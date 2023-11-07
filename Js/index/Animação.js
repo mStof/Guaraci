e334071749f820b9246bbc51a3a90c0d537022ad
Mateus <137577094+mStof@users.noreply.github.com>
2023-11-07 18:41:45 -0300

    Consertando o tempo

--- Js/index/Animação.js
+++ Js/index/Animação.js
@@ -21,7 +21,7 @@ const voltar = () => {
         opacity:0,
         display:'none',
         duration:.3,
-    }, '.sol_svg-=1')
+    })
 
     setTimeout(() => {
         window.scrollTo({top: 0, behavior:'smooth'})
@@ -29,17 +29,17 @@ const voltar = () => {
 
     tlVoltar.to('.sol_svg', {
         scale:1,
-        duration:1,
+        duration:.5,
     })
     tlVoltar.to('.sol_svg', {
         x:0+'%',
-        duration:1,
+        duration:.5,
     })
     tlVoltar.to('.sol_svg', {
         yPercent:'none',
         top:35+"%",
         cursor:'pointer',
-        duration:1,
+        duration:.5,
     })
     tlVoltar.to(divSol, {
         position:'relative',
@@ -49,7 +49,7 @@ const voltar = () => {
     })
     tlVoltar.to('.content_inicial, #sol h1', {
         opacity: 1,
-        duration:1,
+        duration:.5,
     })
 }
 
@@ -57,7 +57,7 @@ const moverSol = () => {
     var tlSol = gsap.timeline()
     tlSol.to('.content_inicial, #sol h1', {
         opacity: 0,
-        duration:1,
+        duration:.5,
     })
     
     tlSol.to(divSol, {
@@ -70,21 +70,21 @@ const moverSol = () => {
         yPercent:-50,
         top:50+'%',
         cursor:'auto',
-        duration:1,
+        duration:.5,
     })
     tlSol.to('.sol_svg', {
-        x:50+'%',
-        duration:1,
+        x:65.5+'%',
+        duration:.5,
     })
     tlSol.to('.sol_svg', {
-        scale:1.4,
-        duration:1,
+        scale:1.68,
+        duration:.5,
     })
-    tlSol.to('nav',{
+    tlSol.to('nav', {
         opacity:1,
         display:'block',
-        duration:.3
-    }, '.sol_svg-=1')
+        duration:.3,
+    })
     tlSol.to('body', {
         background:'var(--primary-blue)',
         overflow:'auto',
@@ -94,3 +94,32 @@ const moverSol = () => {
 
 divSol.addEventListener('click', moverSol)
 document.querySelector('.material-symbols-rounded').addEventListener('click', voltar)
+
+var mq = gsap.matchMedia()
+
+const aniMobile = () => {
+    mq.add("(max-width: 1000px)", () => {
+    var tableTL = gsap.timeline()
+    tableTL.to('body', {
+        background:'var(--primary-blue)',
+        overflow:'auto',
+        duration:0,
+    })
+    tableTL.to('.content_inicial, #sol h1', {
+        opacity: 0,
+        duration:.5,
+    })
+    tableTL.to(divSol, {
+        position:'fixed',
+        y:0,
+        height:100+'vh',
+        duration: 0,
+    })
+    tableTL.to('.sol_svg', {
+        top:-100+'%',
+        scale:1.2,
+        yPercent:50,
+        duration:1,
+    })
+})}
+divSol.addEventListener('click', aniMobile)
